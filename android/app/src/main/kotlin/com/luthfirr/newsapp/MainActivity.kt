de9c7120e978bf94b679927c65b926ec2f2f0781
Luthfirrahman Dzulkarnain <luthfirluthfir@gmail.com>
2023-07-26 01:05:41 +0700

    first commit on newsapp flutter

---
+++ android/app/src/main/kotlin/com/luthfirr/newsapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.luthfirr.newsapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/ArticleWebView.dart';
+import 'package:newsapp/ArticleDetailPage.dart';
+import 'package:newsapp/NewListPage.dart';
+import 'package:newsapp/article.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: NewsListPage.routeName,
+      routes: {
+        NewsListPage.routeName: (context) => const NewsListPage(),
+        ArticleDetailPage.routeName: (context) => ArticleDetailPage(
+          article: ModalRoute.of(context)?.settings.arguments as Article,
+        ),
+        ArticleWebView.routeName: (context) => ArticleWebView(
+          url: ModalRoute.of(context)?.settings.arguments as String,
+        ),
+      },
+    );
+  }
+}
